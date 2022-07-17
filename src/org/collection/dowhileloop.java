175c2e7d5f8a2a05dc1c09f490c925a1195be340
GowthamTC <gowthamtamilmani@gmail.com>
2022-07-17 20:59:58 +0530

    collection reference

---
+++ src/org/collection/dowhileloop.java
@@ -0,0 +1,22 @@
+package org.collection;
+
+import java.util.List;
+
+public class dowhileloop {
+	public static void main(String[] args) {
+		
+		int i=0;
+		
+		do {
+			System.out.println(i);
+			
+			i++;
+			
+		} while (i<=10);
+		
+		}  
+		
+
+			
+
+}
